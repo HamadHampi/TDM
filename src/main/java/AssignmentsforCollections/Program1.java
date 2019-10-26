7af414b1c4233e9792b7913896c561604fb44cf2
Hampi <hampi.hamad@gmail.com>
2019-10-26 10:07:49 +0530

    first commit

---
+++ src/main/java/AssignmentsforCollections/Program1.java
@@ -0,0 +1,18 @@
+package AssignmentsforCollections;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\ProgramData\\Chrome_driver_76.0.3809.68\\Chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://cirrus-alpha.optum.com/");
+	driver.findElement
+
+}
